df7648b5e9aecf4533b0acf5949b4afd032bdcbe
steveWangzx <stevezxwang@gmail.com>
2023-08-21 22:34:35 +0800

    add: convlayer constructor

--- mnist_digit_recognizer/convLayer.hpp
+++ mnist_digit_recognizer/convLayer.hpp
@@ -1,23 +1,120 @@
-#ifndef _conv_layer__
+ï»¿#ifndef _conv_layer__
 #define _conv_layer__
+
+struct dim
+{
+	int width = 0;
+	int height = 0;
+	int tunnel = 0;
+};
 
 class convLayer
 {
 public:
 	convLayer() {};
+	convLayer(int inputWidth, int inputHeight, int filterWidth, int filterHeight, int stride, int padding)
+	{
+		this->inputWidth = inputWidth;
+		this->inputHeight = inputHeight;
+		this->filterHeight = filterHeight;
+		this->filterWidth = filterWidth;
+		this->stride = stride;
+		this->padding = padding;
+
+		// è®¡ç®—è¾“å‡ºçŸ©é˜µå¤§å°
+		// padding - 0
+		// outHeight = (input_height - filter_height + 2 * padding ) / stride + 1
+		// outWidht = (input_width - filter_width + 2 * padding ) / stride + 1
+		this->outputWidth = (this->inputWidth - this->filterWidth + 2 * this->padding) / 
+			this->stride + 1;
+		this->outputHeight = (this->inputHeight - this->filterHeight + 2 * this->padding) / 
+			this->stride + 1;
+
+		// åˆ†é…å†…å­˜
+		x.resize(this->inputWidth * this->inputHeight);		// è¾“å…¥çŸ©é˜µ    
+		y.resize(this->outputHeight * this->outputWidth);	// è¾“å‡ºçŸ©é˜µ
+		w.resize(this->filterWidth * this->filterHeight);	// æƒé‡
+		b.resize(this->outputHeight * this->outputWidth);	// åç½®
+		
+		dx.resize(this->inputWidth * this->inputHeight);
+		dw.resize(this->filterWidth * this->filterHeight);
+		db.resize(this->outputHeight * this->outputWidth);
+		dy.resize(this->outputHeight * this->outputWidth);
+
+		// åˆå§‹åŒ–
+		std::random_device rd_neu;
+		std::default_random_engine eng_neu(rd_neu());
+		std::uniform_real_distribution<float> w_neu(-0.5f, 0.5f);
+		std::uniform_real_distribution<float> b_neu(-0.1f, 0.1f);
+
+		for (int i = 0; i < filterHeight; ++i)
+		{
+			for (int j = 0; j < filterWidth; ++j)
+			{
+				w[i * filterHeight + j] = w_neu(eng_neu);
+				dw[i * filterHeight + j] = 0.0f;
+			}
+		}
+
+		for (int i = 0; i < outputHeight; ++i)
+		{
+			for (int j = 0; j < outputWidth; ++j) 
+			{
+				b[i * outputHeight + j] = 0.0f;
+				db[i * outputHeight + j] = 0.0f;
+			}
+		}
+	}
+
+	dim getOutputDim()
+	{
+		dim temp;
+		temp.width = outputWidth;
+		temp.height = outputHeight;
+		return temp;
+	}
+
+	void setX(vector<float> input)
+	{
+		this->x = input;
+	}
+
+	vector<float> getY()
+	{
+		return this->y;
+	}
+
+	void setDY(vector<float> loss)
+	{
+		this->dy = loss;
+	}
+
+	vector<float> getDX()
+	{
+		return this->dx;
+	}
+
+	void forward()
+	{
+	}
 
 private:
-	int sizeInput;					// ÊäÈësize
-	int sizeNeurals;				// Êä³ösize(Éñ¾­Ôª¸öÊı)
-	Actv actvType;					// ¼¤»îÀàĞÍ
+	int inputWidth;
+	int inputHeight;
+	int filterWidth;
+	int filterHeight;
+	int outputWidth;
+	int outputHeight;
+	int stride;
+	int padding;
 
 	// Foward Parameters
-	vector<float> x;				// ÊäÈëvector - size = sizeInput
-	vector<float> y;				// Êä³övector - size = sizeNeurals
-	vector<float> w;				// È¨ÖØvector - size = sizeInput * sizeNeurals
-	vector<float> b;				// Æ«ÖÃvector - size = sizeNeurals
+	vector<float> x;				// ÃŠÃ¤ÃˆÃ«vector - size = sizeInput
+	vector<float> y;				// ÃŠÃ¤Â³Ã¶vector - size = sizeNeurals
+	vector<float> w;				// ÃˆÂ¨Ã–Ã˜vector - size = sizeInput * sizeNeurals
+	vector<float> b;				// Ã†Â«Ã–Ãƒvector - size = sizeNeurals
 
-	// Backward Parameters ---- Æ«µ¼
+	// Backward Parameters ---- Ã†Â«ÂµÂ¼
 	vector<float> dx;
 	vector<float> dw;
 	vector<float> db;
